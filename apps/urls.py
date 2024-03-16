63ed2bc60fc76a183fe2fbddc44187644492dcd0
msi <meibrohimov.@gmail.com>
2024-03-16 13:00:51 +0500

    new change

--- apps/urls.py
+++ apps/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.views import home, about
 
 urlpatterns = [
-    path('home', home, name='index'),
+    path('home', home, name='home'),
     path('about', about, name='about')
 ]
